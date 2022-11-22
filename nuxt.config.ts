56ef59f4858d82c921c7e3b64db8a75fddd7e8db
Timothee-Picard <timothee@atnos.com>
2022-11-22 16:31:45 +0100

    add title and desc

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    app: {
+        head: {
+            charset: 'utf-16',
+            viewport: 'width=500, initial-scale=1',
+            title: 'Asilenti.fr',
+            meta: [
+                { name: 'description', content: 'Création de site web et design graphiques' }
+            ],
+        }
+    }
 })
